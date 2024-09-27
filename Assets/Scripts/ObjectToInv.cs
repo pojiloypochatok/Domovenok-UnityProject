9600d643cb2760142a26ceaddad4a2848c7e06b2
xzxbtl <xzxbtl@mail.ru>
2024-09-27 06:10:47 +0300

    Stage2

---
+++ Assets/Scripts/ObjectToInv.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+public class ObjectToInv : MonoBehaviour
+{
+    public Vector3 InvPos;
+    public float movespeed = 10;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MoveToInv()
+    {
+        transform.position = InvPos;
+    }
+
+    private void OnMouseDown()
+    {
+        string objectName = gameObject.name;
+        MoveToInv();
+        Debug.Log("Clicked object name: " + objectName);
+    }
+}
